d14a2eeb3a453b766d3ba82562dd14840cefefd9
Laurenchansj <laurenchansj@gmail.com>
2024-06-02 17:34:25 -0600

    refactored Overview page.

--- app/(main)/overview/page.tsx
+++ app/(main)/overview/page.tsx
@@ -1,4 +1,5 @@
 import Card from '@/components/cards/Card';
+import ActivityInOverview from '@/components/cards/ActivityInOverview';
 
 interface CategoryData {
   name: string;
@@ -90,29 +91,7 @@ export default function Overview() {
           <Card title="Total Packages" data={packages} />
           <Card title="Manufacturers" data={manufacturers} />
         </div>
-        <div className="rounded-xl border-4 ml-20 m-10 border-black w-96">
-          <p className="text-xl font-bold p-4 rounded-t-lg bg-[#FFB931]">
-            Recent Activities
-          </p>
-          <div className="border-b-2 border-black bg-white pt-2">
-            <div className="bg-slate-200 p-4 rounded-xl m-4">
-              <p className="text-lg font-bold">New Device added</p>
-              <p className="text-sm">Details</p>
-            </div>
-            <div className="bg-slate-200 p-4 rounded-xl m-4">
-              <p className="text-lg font-bold">Device updated</p>
-              <p className="text-sm">Details</p>
-            </div>
-          </div>
-          <div className="border-b-2 h-36 p-4 border-black bg-white">
-            <p className="font-bold text-xl">Device Assigned</p>
-            <p className="text-sm">Pre-package</p>
-          </div>
-          <div className="border-b-2 h-36 p-4 mb-2 border-black bg-white">
-            <p className="font-bold text-xl">New Device Added</p>
-            <p className="text-sm">Details</p>
-          </div>
-        </div>
+        <ActivityInOverview />
       </div>
     </div>
   );
