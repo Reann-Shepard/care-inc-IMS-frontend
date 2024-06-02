e0547116d7c03f750909e20c0ee7ac82c7e20046
Farhan Batada <farhan.batada@edu.sait.ca>
2024-06-02 08:04:40 -0600

    full show inventory + linking inv-ov and inv-list

--- components/inventory/Inventory-ov_page.tsx
+++ components/inventory/Inventory-ov_page.tsx
@@ -1,11 +1,32 @@
 'use client';
+import dataSet from '@/components/inventory/temp_inv-ovData.json';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-import React from 'react';
-interface InventoryProps {
-  data: { name: string; model: string; quantity: number }[];
+export const metadata = {
+  title: 'Inventory',
+};
+
+interface CategoryData {
+  name: string;
+  model: string;
+  quantity: string;
 }
 
-export default function Inventory({ data }: InventoryProps) {
+export default function InventoryOVPage() {
+  const router = useRouter();
+  const devices: CategoryData[] = dataSet;
+  const [selectedModel, setSelectedModel] = useState<string>();
+
+  const header = ['Company', 'Model', 'Amount'];
+
+  const data = devices.map((device) => [
+    device.name,
+    device.model,
+    device.quantity,
+  ]);
+
   const getBGColor = (quantity: number) => {
     if (quantity <= 2) {
       return 'bg-red-300';
@@ -16,26 +37,54 @@ export default function Inventory({ data }: InventoryProps) {
     }
   };
 
+  useEffect(() => {
+    if (selectedModel) {
+      router.push(`/inventory_list?model=${selectedModel}`);
+    }
+  }, [selectedModel]);
+
   return (
     <div>
-      <div>
-        {data?.map((item, index) => (
-          <div
-            key={index}
-            className="w-full h-10 px-5 bg-gray-100 my-3 flex items-center border-b-2 border-gray-300"
-          >
-            <div className="text-l mr-20 w-1/6">{item.name}</div>
-            <div className="text-l mr-20 w-1/6">{item.model}</div>
-            <div className="text-l w-1/6">
-              <span
-                className={`${getBGColor(item.quantity)} px-2 py-1 rounded`}
-              >
-                {item.quantity}
-              </span>
-            </div>
-          </div>
-        ))}
+      <div className="flex justify-end mb-2 mr-10">
+        <Link href="/inventory/add_inventory">
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-12 rounded">
+            +
+          </button>
+        </Link>
       </div>
+      <table className="table border-2">
+        <thead>
+          <tr className="text-lg text-black bg-gray-200 text-center">
+            {header.map((item) => (
+              <th key={item}>{item}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data &&
+            data.map((row, index) => (
+              <tr
+                key={index}
+                className="text-lg text-center hover "
+                onClick={() => setSelectedModel(row[0] + ' ' + row[1])}
+              >
+                {row.map((item, index) => (
+                  <td key={index} className="">
+                    <div
+                      className={
+                        (index === 2
+                          ? 'w-1/12 m-auto rounded-full bg-opacity-70 border-red-300 '
+                          : '') + (index === 2 ? getBGColor(Number(item)) : '')
+                      }
+                    >
+                      {item}
+                    </div>
+                  </td>
+                ))}
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 }
