d6325f3b7a689d3a42570478fd14ca9cbe25e449
Laurenchansj <laurenchansj@gmail.com>
2024-06-03 07:39:17 -0600

    catching filter selected options in console window, but functionality is not working currently.

--- components/buttons/FilterBtn.tsx
+++ components/buttons/FilterBtn.tsx
@@ -1,19 +1,45 @@
 'use clients';
-import { title } from 'process';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface FilterBtnProps {
-  dataTitle: string[];
+  dataColumnTitle: string[];
+  dataColumnName: string[];
   data: string[][];
+  onFilter: (selectedBoxed: { [key: string]: string[] }) => void;
 }
 
-export default function FilterBtn({ dataTitle, data }: FilterBtnProps) {
+export default function FilterBtn({
+  dataColumnTitle,
+  dataColumnName,
+  data,
+  onFilter,
+}: FilterBtnProps) {
+  const [selectedBoxes, setSelectedBoxes] = useState<{
+    [key: string]: string[];
+  }>({});
   const getUniqueData = (index: number) => {
     return Array.from(new Set(data.map((eachPackage) => eachPackage[index])));
   };
-  const dataUniqueByTitle = dataTitle.map((title, index) =>
+  const dataUniqueByTitle = dataColumnTitle.map((title, index) =>
     getUniqueData(index),
   );
+  // get selectedboxes based on their dataTitle
+  const handleFilterBoxes = (column: string, name: string) => {
+    setSelectedBoxes((prev) => {
+      const prevSelectedBoxes = prev[column] || [];
+      const newSelectedBoxes = prevSelectedBoxes.includes(name)
+        ? prevSelectedBoxes.filter((selected) => selected !== name)
+        : [...prevSelectedBoxes, name];
+      const selectedBoxes = { ...prev, [column]: newSelectedBoxes };
+      // const selectedBoxes = { ...newSelectedBoxes };
+
+      onFilter(selectedBoxes);
+      return selectedBoxes;
+    });
+  };
+  useEffect(() => {
+    console.log(selectedBoxes);
+  }, [selectedBoxes]);
 
   return (
     <div className="flex items-center">
@@ -26,16 +52,21 @@ export default function FilterBtn({ dataTitle, data }: FilterBtnProps) {
           tabIndex={0}
           className="menu dropdown-content z-[1] bg-base-100 shadow rounded-box "
         >
-          {dataTitle.map((title, index) => (
-            <li key={index}>
+          {dataColumnTitle.map((title, titleIndex) => (
+            <li key={titleIndex}>
               <div>
                 <a className="w-20">{title}</a>
-                {dataUniqueByTitle[index].map((name, index) => (
+                {dataUniqueByTitle[titleIndex].map((name, index) => (
                   <label key={index} className="flex w-36">
                     <input
                       type="checkbox"
                       value={name}
-                      // onChange={handleFilterBoxes}
+                      onClick={() => {
+                        handleFilterBoxes(dataColumnName[titleIndex], name);
+                      }}
+                      checked={selectedBoxes[
+                        dataColumnName[titleIndex]
+                      ]?.includes(name)}
                     />
                     <span className="ml-2">{name}</span>
                   </label>
--- components/pages/PackagePage.tsx
+++ components/pages/PackagePage.tsx
@@ -17,6 +17,10 @@ interface Package {
 
 export default function PackagesPage() {
   const [packages, setPackages] = useState<Package[]>([]);
+  const [filteredPackages, setFilteredPackages] = useState<Package[]>([]);
+  const [selectedFilters, setSelectedFilters] = useState<{
+    [key: string]: string[];
+  }>({});
 
   // !@TODO: fetch database column names
   const dataColumnName = [
@@ -145,6 +149,28 @@ export default function PackagesPage() {
   // for filter categories
   const filterHeader = ['Company', 'Model', 'Color'];
 
+  // !!@TODO: filter functionality is not working
+  // for filter data
+  const handlerFilter = (selectedBoxes: { [key: string]: string[] }) => {
+    setSelectedFilters(selectedBoxes);
+  };
+
+  // useEffect(() => {
+  //   // filter data
+  //   const filterPackages = (packages: Package[], selectedFilters: { [key: string]: string[] }) => {
+  //     return packages.filter((eachPackage) => {
+  //       return Object.keys(selectedFilters).every((key) => {
+  //         return selectedFilters[key].length === 0 || selectedFilters[key].includes(eachPackage[key as keyof Package]);
+  //       });
+  //     });
+  //   };
+  //   const sortedPackages = (col: keyof Package) => {
+  //     return [...filteredPackages].sort((a, b) => a[col].localeCompare(b[col]));
+  //   };
+  //   setFilteredPackages(filterPackages(packages, selectedFilters));
+  // }
+  // , [selectedFilters]);
+
   // for data in ListTable
   const data = packages.map((eachPackage) => [
     eachPackage.company,
@@ -166,7 +192,12 @@ export default function PackagesPage() {
             value={header[sortByIndex]}
             onSortBy={handleSortBy}
           />
-          <FilterBtn dataTitle={filterHeader} data={data} />
+          <FilterBtn
+            dataColumnTitle={filterHeader}
+            dataColumnName={dataColumnName}
+            data={data}
+            onFilter={handlerFilter}
+          />
         </div>
         <Link
           href="/packages/add_package"
