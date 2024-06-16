1c6f5efd60fa66455c1e239d20205a8d8e06d6dc
Farhan Batada <farhan.batada@edu.sait.ca>
2024-06-16 07:50:59 -0600

    fetch inventory table from backend

---
+++ services/type/getType.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const getAllTypes = async () => {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+  if (!apiUrl) {
+    console.error('API URL is not found');
+    return [];
+  }
+
+  try {
+    const response = await axios.get(`${apiUrl}/type`);
+    return response.data;
+  } catch (error) {
+    console.error('Failed fetching Type data: ', error);
+    return [];
+  }
+};
+
+export { getAllTypes };
