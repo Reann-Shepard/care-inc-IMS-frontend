aeca85ed3f1b35ac003e27fa38604ce466a968a3
HarryLee87 <haewon.harry.lee@gmail.com>
2024-07-05 14:59:00 -0600

    added postOrdermanufacturer

--- services/orderManufacturer/addOrderManufacturer.ts
+++ services/orderManufacturer/addOrderManufacturer.ts
@@ -17,4 +17,21 @@ const updateOrderManufacturerById = async (id: number, data: any) => {
   }
 };
 
-export { updateOrderManufacturerById };
+const postOrderManufacturerById = async (id: number, data: any) => {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+  if (!apiUrl) {
+    console.error('API URL is not found');
+    return;
+  }
+
+  try {
+    await axios.post(`${apiUrl}/order-manufacturer/${id}`, data);
+    console.log('Successfully posted order manufacturer');
+    console.log(data);
+  } catch (e) {
+    console.error('Failed to post Order Manufacturer data: ', e);
+  }
+};
+
+export { updateOrderManufacturerById, postOrderManufacturerById };
