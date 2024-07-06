f6fffeb59696811d52682faef0e21a427b8ff522
Paige-np <parinthorn.ph@gmail.com>
2024-07-06 17:06:59 -0600

    Fix using  suspense prop properly on inventory page

--- app/(main)/inventory/page.tsx
+++ app/(main)/inventory/page.tsx
@@ -6,8 +6,9 @@ import Inventory from '@/components/inventory/Inventory-ov_page';
 import { Suspense } from 'react';
 
 export default function InventoryOVPage() {
-  return;
-  <Suspense>
-    <Inventory />;
-  </Suspense>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Inventory />;
+    </Suspense>
+  );
 }
