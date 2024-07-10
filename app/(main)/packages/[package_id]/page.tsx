021a4a104cc7105ef2f5cb9fac9006b31bcc17fd
Laurenchansj <laurenchansj@gmail.com>
2024-07-10 12:01:06 -0600

    remove comments

--- app/(main)/packages/[package_id]/page.tsx
+++ app/(main)/packages/[package_id]/page.tsx
@@ -67,17 +67,6 @@ export default function PackageId() {
     fetchDevices();
   }, []);
 
-  // const toDate = (date: string | Date | undefined): string => {
-  //   if (!date) return '';
-  //   if (typeof date === 'string') {
-  //     return date.split('T')[0];
-  //   } else if (date instanceof Date) {
-  //     return date.toISOString().split('T')[0];
-  //   } else {
-  //     throw new Error('Invalid date format.');
-  //   }
-  // };
-
   useEffect(() => {
     const current = `Package ID: ${selectedId}`;
     setCurrentLocation(current);
