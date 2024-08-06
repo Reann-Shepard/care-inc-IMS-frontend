924ab9547889576b2f35c3b5169820e41a9dda12
Laurenchansj <laurenchansj@gmail.com>
2024-08-06 07:34:40 -0600

    fixed bugs for 'no result' showing all the time and sorting by orderDate with fittingDateFilter

--- components/contents/PackagesContent.tsx
+++ components/contents/PackagesContent.tsx
@@ -11,7 +11,7 @@ import {
 import { getAllOrderCustomers } from '../../services/orderCustomer/getOrderCustomer';
 import { useRouter } from 'next/navigation';
 import AddBtn from '../buttons/AddBtn';
-import { set } from 'zod';
+import { set } from 'date-fns';
 
 const ListTable = lazy(() => import('../tables/ListTable'));
 
@@ -20,9 +20,7 @@ export default function PackagesContent() {
   const [allPackages, setAllPackages] = useState<Package[]>([]);
   const [packages, setPackages] = useState<Package[]>([]);
   const [orderCustomers, setOrderCustomers] = useState<OrderCustomer[]>([]);
-  const [sortedData, setSortedData] = useState<
-    (string | number | Date | null)[][]
-  >([]);
+  const [sortedData, setSortedData] = useState<Package[]>([]);
   const [filteredDataFilterValue, setFilteredDataFilterValue] =
     useState<string>('');
   const [loading, setLoading] = useState<boolean>(true);
@@ -66,11 +64,9 @@ export default function PackagesContent() {
   const [selectedFilters, setSelectedFilters] = useState<{
     packageId: (string | number)[];
     clientId: (string | number)[];
-    // fittingDate: string | (string | number)[];
   }>({
     packageId: [],
     clientId: [],
-    // fittingDate: '',
   });
 
   const handleSortBy = (sortBy: string) => {
@@ -85,7 +81,6 @@ export default function PackagesContent() {
       setSelectedFilters({
         packageId: selectedBoxes[header[0]] || [],
         clientId: selectedBoxes[header[1]] || [],
-        // fittingDate: selectedBoxes[header[2]] || '',
       });
     } catch (error) {
       setHasFilterResult(false);
@@ -115,74 +110,30 @@ export default function PackagesContent() {
     });
   };
 
-  const fittingDateFilterData = (value: string) => {
-    let filteredData: Package[] = [];
+  const fittingDateFilterData = (dataToFilter: Package[], value: string) => {
+    let hasResult = false;
 
-    const valueLength = value.length; // to check if the input value is for month or date
-
-    if (valueLength !== 7 && valueLength !== 10) {
+    if (!value || (value.length !== 7 && value.length !== 10)) {
       setFilteredDataFilterValue('');
-      setPackages(allPackages);
-      return [];
+      return dataToFilter;
     }
 
-    allPackages.filter((eachPackage) => {
-      if (
-        eachPackage.fittingDate !== undefined &&
-        eachPackage.fittingDate !== null
-      ) {
+    const filteredData = dataToFilter.filter((eachPackage) => {
+      if (eachPackage.fittingDate) {
         const fittingDate = toDate(eachPackage.fittingDate.toString()).slice(
           0,
-          valueLength,
+          value.length,
         );
         if (fittingDate === value) {
-          filteredData.push(eachPackage);
-        } else {
-          setHasFilterResult(false); // show no result message
+          hasResult = true;
+          return true;
         }
       }
+      return false;
     });
+    setHasFilterResult(hasResult);
     return filteredData;
   };
-
-  useEffect(() => {
-    const fetchSortedFilteredPackagesData = async () => {
-      let sortedFilteredData: Package[] = [];
-
-      if (
-        selectedFilters.packageId.length > 0 ||
-        selectedFilters.clientId.length > 0
-        // selectedFilters.fittingDate.length > 0
-      ) {
-        if (selectedFilters.clientId.find((clientId) => clientId === 'N/A')) {
-          packages.filter((eachPackage) => {
-            if (eachPackage.clientId === null) {
-              sortedFilteredData.push(eachPackage);
-            }
-          });
-        } else {
-          sortedFilteredData = await getAllPackagesSortedFiltered(
-            sortBy,
-            selectedFilters,
-          );
-        }
-      } else {
-        if (sortBy !== 'orderDate') {
-          // orderDate is currently sorted in the front-end
-          sortedFilteredData = await getAllPackagesSortedFiltered(sortBy);
-        }
-      }
-
-      if (filteredDataFilterValue.length > 0) {
-        sortedFilteredData = fittingDateFilterData(filteredDataFilterValue);
-      }
-
-      if (sortedFilteredData !== undefined) {
-        setPackages(sortedFilteredData);
-      }
-    };
-    fetchSortedFilteredPackagesData();
-  }, [sortBy, selectedFilters, filteredDataFilterValue]);
 
   // convert date to string
   const toDate = (date: string | Date | undefined): string => {
@@ -224,17 +175,69 @@ export default function PackagesContent() {
   const data = toTableData(packages);
   const filterData = toTableData(allPackages);
 
-  // when sortBy is 'orderDate', sort by orderDate
   useEffect(() => {
-    if (sortBy == 'orderDate') {
-      const result = [...data].sort((a, b) => {
-        const dateA = a[4] ? a[4].toString() : '';
-        const dateB = b[4] ? b[4].toString() : '';
-        return dateA.localeCompare(dateB);
-      });
-      setSortedData(result as (string | number | Date | null)[][]);
-    }
-  }, [sortBy]);
+    const fetchSortedFilteredPackagesData = async () => {
+      let sortedFilteredData: any = [];
+
+      if (
+        selectedFilters.packageId.length > 0 ||
+        selectedFilters.clientId.length > 0
+      ) {
+        if (selectedFilters.clientId.find((clientId) => clientId === 'N/A')) {
+          packages.filter((eachPackage) => {
+            if (eachPackage.clientId === null) {
+              sortedFilteredData.push(eachPackage);
+            }
+          });
+        } else {
+          sortedFilteredData = await getAllPackagesSortedFiltered(
+            sortBy,
+            selectedFilters,
+          );
+        }
+      } else {
+        if (sortBy !== 'orderDate') {
+          // orderDate is sorted in the front-end
+          sortedFilteredData = await getAllPackagesSortedFiltered(sortBy);
+        } else {
+          // sort by orderDate in the front-end
+          sortedFilteredData = [...allPackages].sort((a, b) => {
+            const dataA = orderCustomers.find(
+              (orderCustomers) => orderCustomers.id === a.orderCustomerId,
+            );
+            const dataB = orderCustomers.find(
+              (orderCustomers) => orderCustomers.id === b.orderCustomerId,
+            );
+            if (dataA && dataB) {
+              return (
+                new Date(dataA.orderDate).getTime() -
+                new Date(dataB.orderDate).getTime()
+              );
+            } else {
+              return 0;
+            }
+          });
+
+          setSortedData(sortedFilteredData);
+        }
+      }
+
+      if (filteredDataFilterValue) {
+        setSortedData(sortedFilteredData);
+        sortedFilteredData = fittingDateFilterData(
+          sortedFilteredData,
+          filteredDataFilterValue,
+        );
+      } else {
+        setHasFilterResult(true);
+      }
+
+      if (sortedFilteredData) {
+        setPackages(sortedFilteredData);
+      }
+    };
+    fetchSortedFilteredPackagesData();
+  }, [sortBy, selectedFilters, filteredDataFilterValue]);
 
   // when row is clicked, go to the detail page
   const handleRowClick = (row: (string | number | Date | null)[]) => {
@@ -270,11 +273,7 @@ export default function PackagesContent() {
         {/* <Suspense fallback={<div>Loading...</div>}> */}
         <ListTable
           header={header}
-          data={
-            sortBy === 'orderDate'
-              ? sortedData
-              : (data as (string | number | Date | null)[][])
-          }
+          data={data as (string | number | Date | null)[][]}
           onClick={handleRowClick}
         />
         {loading && (
