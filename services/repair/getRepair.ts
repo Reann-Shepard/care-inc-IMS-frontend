fe4de7cbf9fd24e0e0bc683e725014c755062392
JIYEON-H <jiyeon.heo0906@gmail.com>
2024-08-06 21:21:19 -0600

    delete comments from the service files

--- services/repair/getRepair.ts
+++ services/repair/getRepair.ts
@@ -1,23 +1,4 @@
-import axios from 'axios';
 import apiClient from '../auth/axios-interceptor';
-
-// const getAllRepairs = async () => {
-//   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-
-//   if (!apiUrl) {
-//     console.error('API URL is not found');
-//     return [];
-//   }
-
-//   try {
-//     const response = await axios.get(`${apiUrl}/repair`);
-//     console.log('Repair data: ', response.data);
-//     return response.data;
-//   } catch (error) {
-//     console.error('Failed fetching Type data: ', error);
-//     return [];
-//   }
-// };
 
 const getAllRepairs = async () => {
   try {
